65b93fa7483f127f596e18cbf99d74c3bd9893c8
an-rahulpandey <rahul.p@applicationnexus.com>
2023-08-17 14:02:57 +0530

    Added dist

---
+++ dist/__tests__/utils/example.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+import { ReactZoomPanPinchProps, ReactZoomPanPinchContentRef } from "../../src";
+export declare const Example: (options: {
+    props?: ReactZoomPanPinchProps | undefined;
+    onRender: () => void;
+    onRenderProps: (ref: ReactZoomPanPinchContentRef) => void;
+}) => React.JSX.Element;
---
+++ dist/src/stories/examples/mini-map/example.d.ts
@@ -0,0 +1,2 @@
+import React from "react";
+export declare const Template: (args: any) => React.JSX.Element;
